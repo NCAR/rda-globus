0d9dcf16c7f5c6d809d975544aa1dc1e1a3cf908
tcram <tcram@ucar.edu>
2016-12-02 17:04:43 -0700

    Debugging

--- cgi-bin/rdaGlobusTransfer.py
+++ cgi-bin/rdaGlobusTransfer.py
@@ -30,10 +30,10 @@ def main():
     form = cgi.FieldStorage()
 
     """ Print HTTP headers and debug info """
-    print_info(form)
+    #print_info(form)
     
-    #task_id = submit_transfer(form)
-    #transfer_status(task_id)
+    task_id = submit_transfer(form)
+    transfer_status(task_id)
 
 def submit_transfer(form):
     """
