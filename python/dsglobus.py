19f52e924f1837d479375d4b15afd205b3553a70
tcram <tcram@ucar.edu>
2017-03-10 15:26:10 -0700

    Added missing exception handler

--- python/dsglobus.py
+++ python/dsglobus.py
@@ -194,7 +194,7 @@ def delete_endpoint_acl_rule(action, data):
 		logging.exception("[delete_endpoint_acl_rule] Totally unexpected GlobusError!")
 		raise
     
-    msg = "[delete_endpoint_acl_rule] {0}. Resource: {1}. Request ID: {2}.".format(result['message'], result['resource'], result['request_id'])
+	msg = "[delete_endpoint_acl_rule] {0}. Resource: {1}. Request ID: {2}.".format(result['message'], result['resource'], result['request_id'])
 	my_logger.info(msg)
 	
 	return msg
