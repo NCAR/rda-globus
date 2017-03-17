ae908a6583bd35c6fc98484db0fbf656713dddec
tcram <tcram@ucar.edu>
2017-03-17 11:22:11 -0600

    Deleted redundant argument definition

--- python/dsglobus.py
+++ python/dsglobus.py
@@ -460,8 +460,6 @@ def parse_input():
 	parser.add_argument('-ne', action="store_true", default=False, help='Do not send notification e-mail.  Default = False.')
 		
 	parser = argparse.ArgumentParser(description="{}".format(sys.argv[0]))
-	parser.add_argument('-ap', action="store_true", default=False, help="Add endpoint permission")
-	parser.add_argument('-rp', action="store_true", default=False, help="Delete endpoint permission")
 	parser.add_argument('-ri', action="store", dest="REQUESTINDEX", type=int, help="dsrqst request index")
 	parser.add_argument('-ds', action="store", dest="DATASETID", help="Dataset ID, specified as dsnnn.n or nnn.n.  Required with the -em argument.")
 	parser.add_argument('-em', action="store", dest="EMAIL", help="User e-mail.  Required with the -ds argument.")
