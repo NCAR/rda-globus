37072530bd60c9f6b84349c1eb3c7700dd5c71ee
tcram <tcram@ucar.edu>
2017-08-09 14:50:27 -0600

    Fixed url

--- cgi-bin/rdaGlobusTransfer.py
+++ cgi-bin/rdaGlobusTransfer.py
@@ -303,7 +303,7 @@ def submit_request(session, form):
 				'dest_path': dest_path
 		        })
 
-	redirect_uri = "{0}/php/dsrqst.php".format(os.environ['HTTP_HOST'])
+	redirect_uri = "https://{0}/php/dsrqst.php".format(os.environ['HTTP_HOST'])
 	r = requests.post(redirect_uri, data=params)
 	if (r.status_code == requests.codes.ok):
 		display_request_message(r, params['dsid'])
