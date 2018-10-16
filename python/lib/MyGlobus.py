d804a29124ab0e6933ac6a1815c82d1d52e296b1
tcram <tcram@ucar.edu>
2018-10-16 08:56:03 -0600

    Deleted transfer token and auth token.  No longer needed.

--- python/lib/MyGlobus.py
+++ python/lib/MyGlobus.py
@@ -45,9 +45,6 @@ GLOBUS_TRANSFER_BASE_URL = 'https://transfer.api.globusonline.org/v0.10/'
 GLOBUS_AUTH_BASE_URL = 'https://auth.globus.org/v2/'
 GLOBUS_APP_URL = 'https://www.globus.org/app/'
 REDIRECT_URI = '/cgi-bin/rdaGlobusTransfer'
-TRANSFER_TOKEN_FILE = 'globus.transfer-token'
-AUTH_TOKEN_FILE = 'globus.auth-token'
-
 DSS_DATA_PATH = '/glade/collections/rda'
 
 """ Endpoint IDs """
@@ -61,24 +58,18 @@ RDA_DATASET_ENDPOINT_LEGACY2 = '2869611a-36aa-11e8-b95e-0ac6873fc732'
 RDA_DSRQST_ENDPOINT_LEGACY1 = 'd20e610e-6d04-11e5-ba46-22000b92c6ec'
 RDA_DSRQST_ENDPOINT_LEGACY1 = '68823254-36aa-11e8-b95e-0ac6873fc732'
 
-transfer_tokenf = open(CLIENT_BASE+TRANSFER_TOKEN_FILE, 'r')
-auth_tokenf = open(CLIENT_BASE+AUTH_TOKEN_FILE, 'r')
 transfer_refresh_tokenf = open(CLIENT_BASE+'globus.transfer-refresh-token', 'r')
 auth_refresh_tokenf = open(CLIENT_BASE+'globus.auth-refresh-token', 'r')
 client_idf = open(CLIENT_BASE+'globus.client-id', 'r')
 client_secretf = open(CLIENT_BASE+'globus.client-secret', 'r')
 private_keyf = open(CLIENT_BASE+'globus.private-key', 'r')
 
-TRANSFER_TOKEN = transfer_tokenf.read().rstrip()
-AUTH_TOKEN = auth_tokenf.read().rstrip()
 TRANSFER_REFRESH_TOKEN = transfer_refresh_tokenf.read().rstrip()
 AUTH_REFRESH_TOKEN = auth_refresh_tokenf.read().rstrip()
 CLIENT_ID = client_idf.read().rstrip()
 CLIENT_SECRET = client_secretf.read().rstrip()
 PRIVATE_KEY = private_keyf.read().rstrip()
 
-transfer_tokenf.close()
-auth_tokenf.close()
 transfer_refresh_tokenf.close()
 auth_refresh_tokenf.close()
 client_idf.close()
@@ -91,8 +82,6 @@ MyGlobus = {
    'url': GLOBUS_TRANSFER_BASE_URL,
    'transfer_refresh_token': TRANSFER_REFRESH_TOKEN,
    'auth_refresh_token': AUTH_REFRESH_TOKEN,
-   'transfer_token': TRANSFER_TOKEN,
-   'auth_token': AUTH_TOKEN,
    'datashare_ep': RDA_DATASET_ENDPOINT,
    'data_request_ep' : RDA_DSRQST_ENDPOINT,
    'datashare_legacy' : 'rda#datashare',
