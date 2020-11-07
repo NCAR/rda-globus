ec341c82239fd3725ea67a0bdd51e92a87cef19d
Thomas Cram <tcram@ucar.edu>
2020-11-06 18:41:33 -0700

    fixed indentation

--- src/python/dsglobus.py
+++ src/python/dsglobus.py
@@ -1249,86 +1249,85 @@ def handle_error(err, **kwargs):
 #=========================================================================================
 def iterable_response_to_dict(iterator):
 	""" Convert Globus paginated/iterable response object to a dict """
-    output_dict = {"DATA": []}
-    for item in iterator:
-        dat = item
-        try:
-            dat = item.data
-        except AttributeError:
-            pass
-        output_dict["DATA"].append(dat)
-    return output_dict
+	output_dict = {"DATA": []}
+	for item in iterator:
+		dat = item
+		try:
+			dat = item.data
+		except AttributeError:
+			pass
+		output_dict["DATA"].append(dat)
+	return output_dict
 
 #=========================================================================================
 def _key_to_keyfunc(k):
-    """
-    We allow for 'keys' which are functions that map columns onto value
-    types -- they may do formatting or inspect multiple values on the
-    object. In order to support this, wrap string keys in a simple function
-    that does the natural lookup operation, but return any functions we
-    receive as they are.
-    """
-    # if the key is a string, then the "keyfunc" is just a basic lookup
-    # operation -- return that
-    if isinstance(k, six.string_types):
+	"""
+	We allow for 'keys' which are functions that map columns onto value
+	types -- they may do formatting or inspect multiple values on the
+	object. In order to support this, wrap string keys in a simple function
+	that does the natural lookup operation, but return any functions we
+	receive as they are.
+	"""
+	# if the key is a string, then the "keyfunc" is just a basic lookup
+	# operation -- return that
+	if isinstance(k, six.string_types):
+		def lookup(x):
+			return x[k]
 
-        def lookup(x):
-            return x[k]
-
-        return lookup
-    # otherwise, the key must be a function which is executed on the item
-    # to produce a value -- return it verbatim
-    return k
+		return lookup
+	# otherwise, the key must be a function which is executed on the item
+	# to produce a value -- return it verbatim
+	return k
 
 #=========================================================================================
 def print_table(iterable, headers_and_keys, print_headers=True):
-    # the iterable may not be safe to walk multiple times, so we must walk it
-    # only once -- however, to let us write things naturally, convert it to a
-    # list and we can assume it is safe to walk repeatedly
-    iterable = list(iterable)
+	# the iterable may not be safe to walk multiple times, so we must walk it
+	# only once -- however, to let us write things naturally, convert it to a
+	# list and we can assume it is safe to walk repeatedly
+	iterable = list(iterable)
 
-    # extract headers and keys as separate lists
-    headers = [h for (h, k) in headers_and_keys]
-    keys = [k for (h, k) in headers_and_keys]
+	# extract headers and keys as separate lists
+	headers = [h for (h, k) in headers_and_keys]
+	keys = [k for (h, k) in headers_and_keys]
 
-    # convert all keys to keyfuncs
-    keyfuncs = [_key_to_keyfunc(key) for key in keys]
+	# convert all keys to keyfuncs
+	keyfuncs = [_key_to_keyfunc(key) for key in keys]
 
-    # use the iterable to find the max width of an element for each column, in
-    # the same order as the headers_and_keys array
-    # use a special function to handle empty iterable
-    def get_max_colwidth(kf):
-        def _safelen(x):
-            try:
-                return len(x)
-            except TypeError:
-                return len(str(x))
+	# use the iterable to find the max width of an element for each column, in
+	# the same order as the headers_and_keys array
+	# use a special function to handle empty iterable
+	def get_max_colwidth(kf):
+		def _safelen(x):
+			try:
+				return len(x)
+			except TypeError:
+				return len(str(x))
 
-        lengths = [_safelen(kf(i)) for i in iterable]
-        if not lengths:
-            return 0
-        else:
-            return max(lengths)
+		lengths = [_safelen(kf(i)) for i in iterable]
+		if not lengths:
+			return 0
+		else:
+			return max(lengths)
 
-    widths = [get_max_colwidth(kf) for kf in keyfuncs]
-    # handle the case in which the column header is the widest thing
-    widths = [max(w, len(h)) for w, h in zip(widths, headers)]
+	widths = [get_max_colwidth(kf) for kf in keyfuncs]
+	# handle the case in which the column header is the widest thing
+	widths = [max(w, len(h)) for w, h in zip(widths, headers)]
 
-    # create a format string based on column widths
-    format_str = " | ".join("{:" + str(w) + "}" for w in widths)
+	# create a format string based on column widths
+	format_str = " | ".join("{:" + str(w) + "}" for w in widths)
 
-    def none_to_null(val):
-        if val is None:
-            return "NULL"
-        return val
+	def none_to_null(val):
+		if val is None:
+			return "NULL"
+		return val
 
-    # print headers
-    if print_headers:
-        click.echo(format_str.format(*[h for h in headers]))
-        click.echo(format_str.format(*["-" * w for w in widths]))
-    # print the rows of data
-    for i in iterable:
-        click.echo(format_str.format(*[none_to_null(kf(i)) for kf in keyfuncs]))
+	# print headers
+	if print_headers:
+		print(format_str.format(*[h for h in headers]))
+		print(format_str.format(*["-" * w for w in widths]))
+	# print the rows of data
+	for i in iterable:
+		print(format_str.format(*[none_to_null(kf(i)) for kf in keyfuncs]))
 
 #=========================================================================================
 """ Set up logging """
