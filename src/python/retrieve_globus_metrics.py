ab000de08d6e03c7445a5b07caaa16a72edb2b4f
Thomas Cram <tcram@ucar.edu>
2022-06-07 11:17:22 -0600

    Moved call to update allusage

--- src/python/retrieve_globus_metrics.py
+++ src/python/retrieve_globus_metrics.py
@@ -73,13 +73,19 @@ def main(filters):
 
 # Get list of successful transfers for each Globus task id.
 	my_logger.debug(__name__+': Getting and adding Globus transfers')
+	endpoint_id = filters['filter_endpoint']
 	for i in range(len(transfer_tasks)):
 		task_id = transfer_tasks[i]['task_id']
 		bytes = transfer_tasks[i]['bytes_transferred']
 		my_logger.debug(__name__+': task_id: '+task_id)
 		data_transfers = get_successful_transfers(task_id)
 		if (len(data_transfers) > 0):
-			add_successful_transfers('gofile', data_transfers, task_id, bytes, filters['filter_endpoint'])
+			add_successful_transfers('gofile', data_transfers, task_id, bytes, endpoint_id)
+
+			# Insert usage from rda#datashare and rda#stratus into table allusage
+			if (endpoint_id == endpoint_id_datashare or endpoint_id == endpoint_id_stratus):
+				update_allusage(task_id)
+
 		else:
 			msg = "[main] Warning: No successful transfers found."
 			my_logger.warning(msg)
@@ -426,10 +432,6 @@ def add_successful_transfers(go_table, data, task_id, bytes, endpoint):
 				my_logger.warning('[add_successful_transfers] Endpoint {0} not found'.format(endpoint))
 				return
 
-	# Insert usage from rda#datashare and rda#stratus into table allusage
-	if ((endpoint == endpoint_id_datashare or endpoint == endpoint_id_stratus) and (len(records) > 0)):
-		update_allusage(task_id)
-
 	if (endpoint == endpoint_id_data_request and len(records) > 0):
 		dsrqst_rec = []
 		pathsplit = records[0]['source_path'].split("/")
@@ -452,7 +454,7 @@ def add_successful_transfers(go_table, data, task_id, bytes, endpoint):
 				myupdt(go_table, dsrqst_rec[0], condition)
 				count_updt += 1
 			else:
-				my_logger.info("[add_successful_transfers] task_id: {0}, rindex {1}: {2} DB record already exists and is up to date.".format(task_id,dsrqst_rec[0]['rindex'],go_table))
+				count_none += 1
 		else:
 			myadd(go_table, dsrqst_rec[0])
 			count_add += 1
@@ -503,7 +505,7 @@ def update_allusage(task_id):
 		org_type = myrec['org_type']
 		country = myrec['country']
 	else:
-		my_logger.warning("[update_allusage] User email {0} not found in table ruser.".format(email))
+		my_logger.warning("[update_allusage] User email {0} not found in table ruser for task ID {1}.".format(email, task_id))
 		return
 	
 	# Get dsid and calculate size.  Query table gofile and handle multiple records, if
