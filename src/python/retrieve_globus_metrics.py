489756a7d3686ac421aca78375534a457fd8fba5
Thomas Cram <tcram@ucar.edu>
2023-11-06 21:32:35 -0700

    updated to use PgDBI

--- src/python/retrieve_globus_metrics.py
+++ src/python/retrieve_globus_metrics.py
@@ -490,11 +490,11 @@ def update_allusage(task_id):
 	count = 0
 	
 	condition = "task_id='{0}'".format(task_id)
-	myrec = pgget('gotask', 'email,completion_time,QUARTER(completion_time)', condition)
+	myrec = pgget('gotask', 'email,completion_time, EXTRACT(QUARTER FROM completion_time) AS quarter', condition)
 	if (len(myrec) > 0):
 		email = myrec['email']
 		completion_time = myrec['completion_time']
-		quarter = myrec['QUARTER(completion_time)']
+		quarter = int(myrec['quarter'])
 	else:
 		my_logger.warning("[update_allusage] Task ID {0} not found.".format(task_id))
 		return
