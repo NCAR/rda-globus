49edcd31d6fa1c153598827ee6f4adb488cf3149
Thomas Cram <tcram@ucar.edu>
2026-03-04 09:14:00 -0700

    Remove stratus endpoint and update path to token config

--- src/python/lib/MyGlobus.py
+++ src/python/lib/MyGlobus.py
@@ -57,7 +57,6 @@ RDA_GLADE_ENDPOINT = '7f0acd80-dfb2-4412-b7b5-ebc970bedf24'
 RDA_QUASAR_ENDPOINT = 'e50caa88-feae-11ea-81a2-0e2f230cc907'
 RDA_QUASAR_DR_ENDPOINT = '4c42c32c-feaf-11ea-81a2-0e2f230cc907'
 
-RDA_STRATUS_ENDPOINT = 'be4aa6a8-9e35-11eb-8a8e-d70d98a40c8d'
 GLOBUS_CGD_ENDPOINT_ID = '11651c26-80c2-4dac-a236-7755530731ac'
 
 GDEX_DATASET_ENDPOINT = 'c4e40965-a024-43d7-bef4-6010f3731b61'
@@ -66,7 +65,7 @@ GDEX_OS_ENDPOINT = '558ad782-80dd-4656-a64a-2245f38a7c9e' # GDEX Boreas object s
 
 
 """ Token storage adapters """
-RDA_QUASAR_TOKEN_STORAGE_ADAPTER = "/glade/u/home/gdexdata/lib/python/globus_rda_quasar_tokens.json"
+RDA_QUASAR_TOKEN_STORAGE_ADAPTER = "/glade/u/home/gdexdata/globus/globus_gdex_quasar_tokens.json"
 
 MyGlobus = {
    'url': GLOBUS_TRANSFER_BASE_URL,
@@ -76,7 +75,6 @@ MyGlobus = {
    'data_request_ep' : RDA_DSRQST_ENDPOINT,
    'datashare_legacy' : 'rda#datashare',
    'data_request_legacy' : 'rda#data_request',
-   'datashare_stratus' : 'rda#stratus',
    'datashare_ep_base' : RDA_DATA_PATH + '/data/',
    'data_request_ep_base' : RDA_DATA_PATH + '/transfer/',
    'host_endpoint_id' : NCAR_HOST_ENDPOINT,
@@ -89,8 +87,7 @@ MyGlobus = {
    'rda_quasar_client_id': RDA_QUASAR_CLIENT_ID,
    'rda_glade_endpoint': RDA_GLADE_ENDPOINT,
    'quasar_endpoint': RDA_QUASAR_ENDPOINT,
-   'quasar_dr_endpoint': RDA_QUASAR_DR_ENDPOINT,
-   'rda_stratus_endpoint': RDA_STRATUS_ENDPOINT
+   'quasar_dr_endpoint': RDA_QUASAR_DR_ENDPOINT
 }
 
 # Endpoint dict mapping endpoint display names and aliases to endpoint IDs
@@ -100,15 +97,12 @@ MyEndpoints = {
     'NCAR RDA GLADE': RDA_GLADE_ENDPOINT,
     'NCAR RDA Quasar': RDA_QUASAR_ENDPOINT,
     'NCAR RDA Quasar DRDATA': RDA_QUASAR_DR_ENDPOINT,
-    'NCAR RDA Stratus': RDA_STRATUS_ENDPOINT,
     'rda-glade': RDA_GLADE_ENDPOINT,
     'rda-quasar': RDA_QUASAR_ENDPOINT,
     'rda-quasar-drdata': RDA_QUASAR_DR_ENDPOINT,
-    'rda-stratus': RDA_STRATUS_ENDPOINT,
     'rda-cgd': GLOBUS_CGD_ENDPOINT_ID,
     'rda#datashare': RDA_DATASET_ENDPOINT,
     'rda#data_request': RDA_DSRQST_ENDPOINT,
-    'rda#stratus': RDA_STRATUS_ENDPOINT,
     'rda#cgd': GLOBUS_CGD_ENDPOINT_ID,
 	'gdex-data': GDEX_DATASET_ENDPOINT,
     'gdex-request': GDEX_DSRQST_ENDPOINT,
